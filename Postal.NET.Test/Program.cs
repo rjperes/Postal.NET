eb4072eec17e83e3970c81a1d17fa34ed1bb9b0f
Ricardo Peres <rjperes@hotmail.com>
2016-03-21 13:53:15 +0000

    Minor improvements

--- Postal.NET.Test/Program.cs
+++ Postal.NET.Test/Program.cs
@@ -1,8 +1,6 @@
 ﻿using PostalConventions.NET;
 using PostalRX.NET;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reactive.Linq;
 using System.Threading;
 using PostalWhen.NET;
--- Postal.NET/BoxExtensions.cs
+++ Postal.NET/BoxExtensions.cs
@@ -4,8 +4,39 @@ namespace Postal.NET
 {
     public static class BoxExtensions
     {
+        public static void Once(this IBox box, string channel, string topic, Action<Envelope> subscriber, Func<Envelope, bool> condition = null)
+        {
+            if (box == null)
+            {
+                throw new ArgumentNullException("box");
+            }
+
+            if (subscriber == null)
+            {
+                throw new ArgumentNullException("subscriber");
+            }
+
+            IDisposable subscription = null;
+
+            subscription = box.Subscribe(channel, topic, (env) =>
+            {
+                subscriber(env);
+
+                if (subscription != null)
+                {
+                    subscription.Dispose();
+                    subscription = null;
+                }
+            }, condition);
+        }
+
         public static void MultiPublish(this IBox box, string channel, string topic, params object[] datas)
         {
+            if (box == null)
+            {
+                throw new ArgumentNullException("box");
+            }
+
             foreach (var data in datas)
             {
                 box.Publish(channel, topic, data);
@@ -14,6 +45,16 @@ namespace Postal.NET
 
         public static void MultiPublish(this IBox box, string channel, string topic, Func<object> factory)
         {
+            if (box == null)
+            {
+                throw new ArgumentNullException("box");
+            }
+
+            if (factory == null)
+            {
+                throw new ArgumentNullException("factory");
+            }
+
             for (var data = factory(); data != null; data = factory())
             {
                 box.Publish(channel, topic, data);
@@ -22,11 +63,21 @@ namespace Postal.NET
 
         public static IDisposable Subscribe<T>(this IBox box, string channel, string topic, Action<T> subscriber)
         {
+            if (box == null)
+            {
+                throw new ArgumentNullException("box");
+            }
+
             return box.Subscribe(channel, topic, (env) => subscriber((T) env.Data), (env) => env.Data is T);
         }
 
         public static IChannel Channel(this IBox box, string channel)
         {
+            if (box == null)
+            {
+                throw new ArgumentNullException("box");
+            }
+
             if (string.IsNullOrWhiteSpace(channel) == true)
             {
                 throw new ArgumentNullException("channel");
@@ -37,16 +88,31 @@ namespace Postal.NET
 
         public static ITopic AnyChannelAndTopic(this IBox box)
         {
+            if (box == null)
+            {
+                throw new ArgumentNullException("box");
+            }
+
             return AnyChannel(box).AnyTopic();
         }
 
         public static IChannel AnyChannel(this IBox box)
         {
+            if (box == null)
+            {
+                throw new ArgumentNullException("box");
+            }
+
             return Channel(box, Postal.All);
         }
 
         public static ITopic AnyTopic(this IChannel channel)
         {
+            if (channel == null)
+            {
+                throw new ArgumentNullException("channel");
+            }
+
             return channel.Topic(Postal.All);
         }
     }
